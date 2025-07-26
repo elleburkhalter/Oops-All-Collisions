535908e68450497f98f2048198a5beceedd00b3c
logandapp <logandapp@gmail.com>
2025-07-25 21:36:50 -0400

    Saved draw_terrain for the far future; focusing on agents and projectiles.

--- include/renderer/RendererInterface.h
+++ include/renderer/RendererInterface.h
@@ -18,11 +18,11 @@ class RendererInterface
     // ----- Entities -----
     virtual void draw_agent(const Agent& agent) = 0;
     virtual void draw_projectile(const Projectile& projectile) = 0;
-    virtual void draw_terrain(const Terrain& terrain) = 0;
+    // virtual void draw_terrain(const Terrain& terrain) = 0;  // Comes later probably? Haven't thought through this.
 
     // ----- Debug visualization -----
     virtual void draw_bounding_box(const BoundingBox& bounding_box) = 0;
     virtual void draw_text(const Text& text) = 0;
-}
+};
 
 #endif //RENDERERINTERFACE_H
