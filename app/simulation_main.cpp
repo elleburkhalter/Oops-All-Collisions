8e60b7cf0f088e9041c1a2f33aad5b7f6efa9b73
elleburkhalter <elle.burkhalter@ufl.edu>
2025-07-29 17:04:47 -0400

    Add wlecome screen to simulation_main.cpp

--- app/simulation_main.cpp
+++ app/simulation_main.cpp
@@ -26,31 +26,53 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 2500;
+    const int screenHeight = 1500;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);    // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+    bool onWelcomeScreen = true;
+
     EntityABC exampleGuy1(screenWidth/2, screenHeight/2, 10);
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
+        if (onWelcomeScreen) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                onWelcomeScreen = false;
+            }
+        }
         //----------------------------------------------------------------------------------
-        exampleGuy1.playerControl();
+        else {
+            exampleGuy1.playerControl();
+        }
         //----------------------------------------------------------------------------------
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
+        if (onWelcomeScreen)
+        {
+            DrawText("Oops! All Collisions",(screenWidth - MeasureText("Oops! All Collisions", 120)) / 2,screenHeight / 2 - 250,120, BLUE);
+            DrawText("Created by Logan Dapp, Derrick Davison, and Elle Burkhalter",(screenWidth - MeasureText("Created by Logan Dapp, Derrick Davison, and Elle Burkhalter", 50)) / 2,screenHeight / 2 - 120, 50, GREEN);
+            DrawText("Click anywhere to begin...",(screenWidth - MeasureText("Click anywhere to begin...", 40)) / 2,screenHeight / 2 + 40,40, ORANGE);
+            //Border
+            DrawRectangleLinesEx((Rectangle) {screenWidth / 2 - 1000,screenHeight / 2 - 500, 2000, 850 },6, SKYBLUE);
+            //Effects
+            DrawCircleGradient(screenWidth/4, screenHeight/2 + 200, 100, PINK, RED);
+            DrawCircleGradient(screenWidth/8, screenHeight/2 + 600, 350, YELLOW, ORANGE);
+            DrawCircleGradient(screenWidth - 350, screenHeight - 1200, 200, MAGENTA, DARKPURPLE);
+            DrawCircleGradient(screenWidth - 700, screenHeight - 200, 150, SKYBLUE, DARKBLUE);
+        } else {
             DrawText("move dude with keys", 10, 10, 20, DARKGRAY);
             exampleGuy1.drawEntity();
-
             //DrawCircleV(ballPosition, 50, MAROON);
+        }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
