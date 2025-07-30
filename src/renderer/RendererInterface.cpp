c2239f830436f95702f156c216eb0285e8277e67
Vengeful Nostril <44213227+mcnairrobotics@users.noreply.github.com>
2025-07-30 15:40:05 -0400

    Update RendererInterface.cpp

--- src/renderer/RendererInterface.cpp
+++ src/renderer/RendererInterface.cpp
@@ -1,27 +1,27 @@
 #include <renderer/RendererInterface.h>
-virtual bool is_open(){
+    RendererInterface::is_open(){
         
     }
-    virtual void start_frame(){
+    RendererInterface::start_frame(){
 
     }
-    virtual void finish_frame(){
+    RendererInterface::finish_frame(){
 
     }
 
     // ----- Entities -----
-    virtual void draw_agent(const AgentInterface& agent){
+    RendererInterface::draw_agent(const AgentInterface& agent){
 
     }
-    virtual void draw_projectile(const ProjectileInterface& projectile){
+    RendererInterface::draw_projectile(const ProjectileInterface& projectile){
 
     }
     // virtual void draw_terrain(const Terrain& terrain) = 0;  // Comes later probably? Haven't thought through this.
 
     // ----- Debug visualization -----
-    virtual void draw_bounding_box(const BoundingBox& bounding_box){
+    RendererInterface::draw_bounding_box(const BoundingBox& bounding_box){
 
     }
-    virtual void draw_text(const Text& text){
+    RendererInterface::draw_text(const Text& text){
 
     }
