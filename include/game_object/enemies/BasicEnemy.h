c09b94927ced7a839dcd04c558f00ba669c77ad5
logandapp <logandapp@gmail.com>
2025-07-29 21:30:41 -0400

    Added first concrete implementation (and probably only implementation) of the interfaces for the project, but we'll see.

---
+++ include/game_object/enemies/BasicEnemy.h
@@ -0,0 +1,12 @@
+#ifndef BASICENEMY_H
+#define BASICENEMY_H
+
+#include <game_object/interfaces/EntityInterface.h>
+#include <game_object/interfaces/AgentInterface.h>
+
+class BasicEnemy : public EntityInterface, public AgentInterface
+{
+    // TODO: Implement interface methods.
+};
+
+#endif //BASICENEMY_H
