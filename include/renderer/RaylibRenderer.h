8c35ee178592c87b0a6623ae042fbdd6aefb5542
logandapp <logandapp@gmail.com>
2025-07-29 20:14:16 -0400

    Started RaylibRenderer.h...

--- include/renderer/RaylibRenderer.h
+++ include/renderer/RaylibRenderer.h
@@ -2,7 +2,9 @@
 #ifndef RAYLIBRENDERER_H
 #define RAYLIBRENDERER_H
 
-// This will implement IRenderer interface to allow for visualization with
+#include <raylib.h>
+
+// This will implement RendererInterface interface to allow for visualization with
 // Raylib.
 
 #endif //RAYLIBRENDERER_H
