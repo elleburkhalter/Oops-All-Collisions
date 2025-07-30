05a501410bf596dfca9627cf36f528263f1ba5ba
Vengeful Nostril <44213227+mcnairrobotics@users.noreply.github.com>
2025-07-30 15:33:45 -0400

    Fleshed out RendererInterface.cpp

--- src/renderer/RendererInterface.cpp
+++ src/renderer/RendererInterface.cpp
@@ -1,1 +1,26 @@
+    virtual bool is_open(){
+        
+    }
+    virtual void start_frame(){
 
+    }
+    virtual void finish_frame(){
+
+    }
+
+    // ----- Entities -----
+    virtual void draw_agent(const AgentInterface& agent){
+
+    }
+    virtual void draw_projectile(const ProjectileInterface& projectile){
+
+    }
+    // virtual void draw_terrain(const Terrain& terrain) = 0;  // Comes later probably? Haven't thought through this.
+
+    // ----- Debug visualization -----
+    virtual void draw_bounding_box(const BoundingBox& bounding_box){
+
+    }
+    virtual void draw_text(const Text& text){
+
+    }
