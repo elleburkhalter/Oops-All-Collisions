8307b02b6c93dd3dc6a9c448e7d77ae248b14654
Vengeful Nostril <44213227+mcnairrobotics@users.noreply.github.com>
2025-07-30 15:19:12 -0400

    Update RaylibRenderer.cpp

--- src/renderer/RaylibRenderer.cpp
+++ src/renderer/RaylibRenderer.cpp
@@ -1,3 +1,68 @@
 //
 // Created by logan on 7/29/2025.
 //
+#include <raylib.h>
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 2500;
+    const int screenHeight = 1500;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+
+    SetTargetFPS(60);    // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+    bool onWelcomeScreen = true;
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        if (onWelcomeScreen) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                onWelcomeScreen = false;
+            }
+        }
+        //----------------------------------------------------------------------------------
+        else {
+        }
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        if (onWelcomeScreen)
+        {
+            DrawText("Oops! All Collisions",(screenWidth - MeasureText("Oops! All Collisions", 120)) / 2,screenHeight / 2 - 250,120, BLUE);
+            DrawText("Created by Logan Dapp, Derrick Davison, and Elle Burkhalter",(screenWidth - MeasureText("Created by Logan Dapp, Derrick Davison, and Elle Burkhalter", 50)) / 2,screenHeight / 2 - 120, 50, GREEN);
+            DrawText("Click anywhere to begin...",(screenWidth - MeasureText("Click anywhere to begin...", 40)) / 2,screenHeight / 2 + 40,40, ORANGE);
+            //Border
+            DrawRectangleLinesEx((Rectangle) {screenWidth / 2 - 1000,screenHeight / 2 - 500, 2000, 850 },6, SKYBLUE);
+            //Effects
+            DrawCircleGradient(screenWidth/4, screenHeight/2 + 200, 100, PINK, RED);
+            DrawCircleGradient(screenWidth/8, screenHeight/2 + 600, 350, YELLOW, ORANGE);
+            DrawCircleGradient(screenWidth - 350, screenHeight - 1200, 200, MAGENTA, DARKPURPLE);
+            DrawCircleGradient(screenWidth - 700, screenHeight - 200, 150, SKYBLUE, DARKBLUE);
+        } else {
+            DrawText("move dude with keys", 10, 10, 20, DARKGRAY);
+            //DrawCircleV(ballPosition, 50, MAROON);
+        }
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
