76cf1256c077b7b971b518bbd26fe1f13d1fa488
logandapp <logandapp@gmail.com>
2025-07-30 21:27:30 -0400

    Updated the default container to not duplicate the collisions.

--- src/data_structures/ContainerInterface.cpp
+++ src/data_structures/ContainerInterface.cpp
@@ -1,13 +1,22 @@
+#include <unordered_set>
 #include <data_structures/ContainerInterface.h>
 
-std::vector<std::pair<EntityInterface&, EntityInterface&>> ContainerInterface::get_all_collisions() const
+std::list<std::pair<EntityInterface&, EntityInterface&>> ContainerInterface::get_all_collisions() const
 {
-    std::vector<std::pair<EntityInterface&, EntityInterface&>> active_collisions;
+    std::list<std::pair<EntityInterface&, EntityInterface&>> active_collisions;
+    const auto table_size = static_cast<size_t>(static_cast<double>(this->get_entity_count()) * AVERAGE_COLLISIONS_PER_ENTITY / TARGET_LOAD_FACTOR);
+    std::unordered_set<collision_record, collision_record_hasher> visited_collisions{};
+    visited_collisions.reserve(table_size);
+
     for (EntityInterface& entity1 : this->get_all_entities())
     {
         for (EntityInterface& entity2 : this->get_collisions(entity1))
         {
             if (&entity1 == &entity2) continue;  // check for self-collision.
+
+            const collision_record collision(&entity1, &entity2);
+            if (visited_collisions.contains(collision)) continue;
+            visited_collisions.insert(collision);
             active_collisions.emplace_back(entity1, entity2);
         }
     }
