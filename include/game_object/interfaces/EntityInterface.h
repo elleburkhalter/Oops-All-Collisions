735ea78b3b1be20be484f8524be30b07eacdc93c
logandapp <logandapp@gmail.com>
2025-08-01 11:33:10 -0400

    Added void ptr casting and tagging to entities.

--- include/game_object/interfaces/EntityInterface.h
+++ include/game_object/interfaces/EntityInterface.h
@@ -16,6 +16,8 @@ public:
     [[nodiscard]] virtual inline Point get_location() const = 0;  // should be called by renderer to plot point
     [[nodiscard]] virtual inline Point get_velocity() const = 0;
     [[nodiscard]] ColliderInterface& get_collider() const { return collider; }
+    template <typename T>
+    [[nodiscard]] std::shared_ptr<T> get_tag() { return std::static_pointer_cast<T>(this->tag); };
 
     // ----- Setters -----
     void set_movement_enabled() { flags |= EntityFlags::MovementEnabled; }
@@ -29,6 +31,9 @@ public:
     virtual inline void set_position(Point pos) = 0;
     virtual inline void set_velocity(Point vel) = 0;
 
+    template <typename T>
+    void set_tag(const std::shared_ptr<T>& ptr) { tag = std::static_pointer_cast<void>(ptr); };
+
     // ----- Checkers -----
     [[nodiscard]] bool movement_enabled() const { return flags & EntityFlags::MovementEnabled; }
     [[nodiscard]] bool collision_enabled() const { return flags & EntityFlags::CollisionEnabled; }
@@ -39,6 +44,7 @@ public:
 private:
     ColliderInterface& collider;
     uint8_t flags = EntityFlags::MovementEnabled | EntityFlags::CollisionEnabled;
+    std::weak_ptr<void> tag{};
 };
 
 #endif //ENTITYINTERFACE_H
