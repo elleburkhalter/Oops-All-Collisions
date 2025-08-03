9d55b196c5e634f05e70c0898538e40da545fe78
logandapp <logandapp@gmail.com>
2025-08-03 12:57:02 -0400

    Transitioned from regular text to a textinterface so that we can make it independent of raylib rendering.

---
+++ include/renderer/ui_object/text/NullText.h
@@ -0,0 +1,24 @@
+#ifndef NULLTEXT_H
+#define NULLTEXT_H
+
+#include <renderer/ui_object/text/TextInterface.h>
+
+class NullText final : public TextInterface
+{
+public:
+    NullText(): text("Default Text") {};
+    NullText(std::string text, const size_t _, const Point __ = {0, 0}): text(std::move(text)) {};
+
+    size_t get_font_size() const override { return 0; }
+    std::string get_text() const override { return text; }
+    Point get_relative_position() const override { return {0, 0}; }
+    TextLocation get_anchor() const override { return TextLocation::UL_RELATIVE; };
+    TextAlignment get_alignment() const override { return TextAlignment::UL; };
+    Point get_absolute_position(const OopsBoundingBox& _) const override { return {0, 0}; };
+    Point get_absolute_position() const override { return {0, 0}; };
+
+private:
+    std::string text;
+};
+
+#endif //NULLTEXT_H
