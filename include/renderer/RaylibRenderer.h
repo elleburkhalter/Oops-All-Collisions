f731b5992a247014100ef57373c839b07cf8105a
logandapp <logandapp@gmail.com>
2025-08-03 11:31:26 -0400

    Set up the renderer to follow the interface laid out in RendererInterface.

--- include/renderer/RaylibRenderer.h
+++ include/renderer/RaylibRenderer.h
@@ -3,12 +3,19 @@
 #define RAYLIBRENDERER_H
 
 #include <raylib.h>
+#include <renderer/RendererInterface.h>
 
 // This will implement RendererInterface interface to allow for visualization with
 // Raylib.
-class RaylibRenderer
+class RaylibRenderer final : public RendererInterface
 {
-    public:
-    virtual int start();
+    void draw_agent(const AgentInterface& agent) override;
+    void draw_projectile(const ProjectileInterface& projectile) override;
+
+    void draw_box(const OopsBoundingBox& bounding_box) override;
+    void draw_point(Point point) override;
+    void draw_circle(Ball ball) override;
+    void draw_text(const Text& text) override;
+
 };
 #endif //RAYLIBRENDERER_H
