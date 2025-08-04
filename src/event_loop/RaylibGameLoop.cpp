c932ec490676c510c2eb745f78459263904448d7
logandapp <logandapp@gmail.com>
2025-08-03 23:58:45 -0400

    Hacked in state checking to the mouse handler; I did not think about that while implementing. But because the mouse is relatively simple, it will do.

--- src/event_loop/RaylibGameLoop.cpp
+++ src/event_loop/RaylibGameLoop.cpp
@@ -12,9 +12,11 @@ void RaylibGameLoop::initialize()
 {
     this->instantiate_entities(NUM_ENTITIES);
 
-    this->mouse_handler.add_while_left([&](const double x, const double y) -> void { this->renderer.move_view_area(x, y); });
-    this->mouse_handler.add_off_left([&](const double _, const double __) -> void { this->renderer.unset_last_point(); });
-    this->mouse_handler.add_on_scroll([&](const double dy) -> void { this->renderer.zoom_view_area(dy); });
+    this->mouse_handler.add_while_left([&](const double x, const double y) -> void { if (this->get_state() == GameState::ACTIVE_GAME_STATE) this->renderer.move_view_area(x, y); });
+    this->mouse_handler.add_off_left([&](const double _, const double __) -> void { if (this->get_state() == GameState::ACTIVE_GAME_STATE) this->renderer.unset_last_point(); });
+    this->mouse_handler.add_on_scroll([&](const double dy) -> void { if (this->get_state() == GameState::ACTIVE_GAME_STATE) this->renderer.zoom_view_area(dy); });
+
+    this->mouse_handler.add_on_left([&](const double _, const double __) -> void { if (this->get_state() == GameState::TITLE_SCREEN_STATE) this->set_state(GameState::ACTIVE_GAME_STATE); });
 }
 void RaylibGameLoop::update_hook()
 {
