c9d1726b18e3d06d309443008d449c74814e384b
logandapp <logandapp@gmail.com>
2025-08-03 23:55:07 -0400

    Updated the simulation main.

--- app/simulation_main.cpp
+++ app/simulation_main.cpp
@@ -1,28 +1,18 @@
 
-// This is the program that will build with --DWITH_SFML=ON and
-// run with --visualize=1
-/*******************************************************************************************
-*
-*   raylib [core] example - Keyboard input
-*
-*   Example complexity rating: [★☆☆☆] 1/4
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2014-2025 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
 
-#include <raylib.h>
+#include <event_loop/RaylibGameLoop.h>
 #include <renderer/RaylibRenderer.h>
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+#include <mouse_handler/RaylibMouseHandler.h>
+#include <data_structures/NaiveLinear.h>
+
 int main(){
-    RaylibRenderer renderer;
-    renderer.start();
-    
+    NaiveLinear data_structure{};
+    RaylibRenderer raylib_renderer{};
+    RaylibMouseHandler raylib_mouse_handler{};
+    RaylibGameLoop raylib_game_loop{data_structure, raylib_renderer, raylib_mouse_handler};
+
+    raylib_game_loop.initialize();
+    raylib_game_loop.run();
+
+    return 0;
 }
